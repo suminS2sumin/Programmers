183764061b52d52467bd7c698880377fbf8b232c
suminS2sumin <sm619616@naver.com>
2025-03-23 12:55:05 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 9.34 MB -BaekjoonHub

--- Python3/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ Python3/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -2,7 +2,4 @@ def solution(a, b):
     answer1 = str(a) + str(b)
     answer2 = str(b) + str(a)
     
-    if int(answer1) >= int(answer2):
-        return int(answer1)
-    else:
-        return int(answer2)
+    return int(max(answer1, answer2))
