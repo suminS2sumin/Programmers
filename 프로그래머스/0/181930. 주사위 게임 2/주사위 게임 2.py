f089e1e0c011d41428a2df08f3e24ee45aaa0ce1
suminS2sumin <sm619616@naver.com>
2025-04-01 16:25:27 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,13 @@
+def solution(a, b, c):
+    answer = 0
+    
+    if a != b != c:
+        answer = a + b + c
+    
+    if (a == b and a != c) or (a == c and a != b) or (b == c and b != a):
+        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
+        
+    if a == b == c:
+        answer = (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
+        
+    return answer
