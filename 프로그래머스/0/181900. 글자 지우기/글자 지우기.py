34bb494752cccfe2dd2a4d5efd065a6c1e5133c7
suminS2sumin <sm619616@naver.com>
2025-04-07 16:32:08 +0900

    [level 0] Title: 글자 지우기, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.py
@@ -1,10 +1,8 @@
 def solution(my_string, indices):
-    answer = ''
     sort_indices = sorted(indices, reverse = True)
     l_string = list(my_string)
     
     for i in sort_indices:
         del l_string[i]
         
-    answer = ''.join(l_string)
-    return answer
+    return ''.join(l_string)
